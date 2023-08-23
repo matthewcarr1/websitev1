dd7e84561d5edd9b2ba5dfa013f1a306af3e4390
Matt Carr <matthewcarr428@gmail.com>
2023-08-23 17:45:09 -0400

    Adding some nav bar additions and some style changes. Still cant get the background image behind the nav bar to format with the display flex property.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 window.onload = function() {
     const imageGallery = document.getElementById("imageGallery");
+    const imageGalleryC1 = document.getElementById("imageGalleryC1");
+    const imageGalleryC2 = document.getElementById("imageGalleryC2");
+    const imageGalleryC3 = document.getElementById("imageGalleryC3");
     const imageFolder = "images/"; // Path to your image folder
     const imageCount = 13; // Number of images
 
@@ -8,6 +11,13 @@ window.onload = function() {
         img.src = imageFolder + i + ".jpg"; // Change the image file naming if needed
         img.alt = "Image " + i;
         img.classList.add("gallery-image");
-        imageGallery.appendChild(img);
+        if (i<=(i/3)+1) {
+            imageGalleryC1.appendChild(img);
+        } else if (i>(i/3) && i<=(2*i/3)) {
+            imageGalleryC2.appendChild(img);
+        } else {
+            imageGalleryC3.appendChild(img);
+        }
+        // imageGallery.appendChild(img);
     }
 };
